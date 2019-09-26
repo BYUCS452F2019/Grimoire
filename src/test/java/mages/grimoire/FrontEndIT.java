5203b30e30df255d586db7cb917be9d723a6aa71
Ben Welker <ringmaster217@gmail.com>
2019-09-26 08:59:54 -0600

    changed the context root

--- src/test/java/mages/grimoire/FrontEndIT.java
+++ src/test/java/mages/grimoire/FrontEndIT.java
@@ -3,12 +3,11 @@ package mages.grimoire;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import mages.grimoire.Application;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -25,7 +24,7 @@ public class FrontEndIT {
   @Test
   public void testIndex() {
     RestTemplate rest = new RestTemplate();
-    String uri = "http://localhost:" + port + "/grimoire";
+    String uri = "http://localhost:" + port + "/";
     ResponseEntity<String> response = rest.getForEntity(uri, String.class);
 
     // Verify that the correct response code was sent.
