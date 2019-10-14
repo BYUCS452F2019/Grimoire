70e353b9e17492adbe3907f8d8cae432566b5b84
Allanon <allanon521@gmail.com>
2019-10-14 11:45:20 -0600

    home page should be ready to hook up to the server

--- src/main/angular/src/app/home/home.component.ts
+++ src/main/angular/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    books = [];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    createSpell() {}
+
+    search(bookId) {
+    }
+
+    newBook(){}
 
 }
