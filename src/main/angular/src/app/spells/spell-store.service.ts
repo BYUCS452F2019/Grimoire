f64494895fde9707fcc903054e1302a130ddd6fc
Allanon <allanon521@gmail.com>
2019-10-28 09:24:52 -0600

    cleaned up some of the view

--- src/main/angular/src/app/spells/spell-store.service.ts
+++ src/main/angular/src/app/spells/spell-store.service.ts
@@ -8,32 +8,7 @@ export class SpellStoreService {
     @observable currentId;
 
     constructor(private spellApi: SpellApiService){
-        // TODO make this use the actual api instead of the dummy data
-        // this.spellApi.getAllSpells().subscribe((spells: any) => this.allSpells = spells);
-        this.allSpells = [
-        {
-            book: 1,
-            castingTime: '1 action',
-            classType: 'Bard',
-            concentration: true,
-            damage: '1d6',
-            damageType: 'Acid',
-            description: 'I do not know I just made this up',
-            duration: 0,
-            higherLevels: '',
-            level: 1,
-            material: 'a drop of spit',
-            range: 30,
-            ritual: false,
-            saving_throw: '',
-            school: 'Abjuration',
-            somatic: true,
-            spellId: 0,
-            spellName: 'Stinging Spit',
-            target: '1 creature',
-            verbal: false
-        },
-    ];
+        this.spellApi.getAllSpells().subscribe((spells: any) => this.allSpells = spells);
     }
 
     @computed get currentSpell() {
