c07eea7d48d95b67295cddaaa11ee191351396ef
Allanon <allanon521@gmail.com>
2019-10-30 13:37:36 -0600

    fixed the spell filtering in a book

--- src/main/angular/src/app/spells/spell-store.service.ts
+++ src/main/angular/src/app/spells/spell-store.service.ts
@@ -1,21 +1,24 @@
 import {observable, computed, action } from 'mobx';
 import { Injectable } from '@angular/core';
 import { SpellApiService, SpellBookApiService } from '../services';
+import * as _ from 'lodash';
 
 @Injectable()
 export class SpellStoreService {
     @observable allSpells = [];
     @observable currentId;
     @observable currentBook;
-    @observable filterName;
+    @observable filterName = '';
     @observable filterLevel;
-    @observable filterClass;
-    @observable filterSchool;
-    @observable filterDamageType;
+    @observable filterClass = '';
+    @observable filterSchool = '';
+    @observable filterDamageType = '';
 
 
     constructor(private spellApi: SpellApiService, private bookApi: SpellBookApiService){
-        this.spellApi.getAllSpells().subscribe((spells: any) => this.allSpells = spells);
+        this.spellApi.getAllSpells().subscribe((spells: any) => { 
+            this.allSpells = spells
+        });
     }
 
     @computed get currentSpell() {
@@ -28,27 +31,27 @@ export class SpellStoreService {
         if (!this.currentBook) return;
         return this.currentBook.spells.filter((spell) => {
             if (this.filterName) {
-                if (!spell.spellName.includes(this.filterName)) {
+                if (!spell.spellName.toLowerCase().includes(this.filterName.toLowerCase())) {
                     return false;
                 }
             }
-            if (this.filterLevel) {
+            if (!_.isNil(this.filterLevel)) {
                 if (spell.level !== this.filterLevel) {
                     return false;
                 }
             }
             if (this.filterClass) {
-                if (spell.classType.evry((spellClass) => spellClass !== this.filterClass)) {
+                if (spell.classType.every((spellClass) =>  spellClass !== this.filterClass.toString())) {
                     return false;
                 }
             }
             if (this.filterSchool) {
-                if (spell.school !== this.filterSchool) {
+                if (spell.school !== this.filterSchool.toString()) {
                     return false;
                 }
             }
             if (this.filterDamageType) {
-                if (spell.damageType !== this.filterDamageType) {
+                if (spell.damageType !== this.filterDamageType.toString()) {
                     return false;
                 }
             }
@@ -66,9 +69,9 @@ export class SpellStoreService {
             }
         } else {
             this.bookApi.getSpellBook(bookId).subscribe((book) => {
-                debugger;
                 this.currentBook = book
             })
         }
     }
+
 }
